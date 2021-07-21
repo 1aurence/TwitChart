ff07fca0bb75972be596cf1cd258f2e775716fca
Hall <lahall@BOSTON.local>
2021-07-21 15:11:15 -0400

    added twit.js

--- server.js
+++ server.js
@@ -1,7 +1,31 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = 3000;
 const ejs = require("ejs");
+const Twit = require("twit");
+
+const T = new Twit({
+  consumer_key: process.env["CONSUMER-KEY"],
+  consumer_secret: process.env["CONSUMER-SECRET-KEY"],
+  access_token: process.env["ACCESS-TOKEN"],
+  access_token_secret: process.env["ACCESS-TOKEN-SECRET"],
+  timeout_ms: 60 * 1000,
+  strictSSL: true,
+});
+
+T.get(
+  "search/tweets",
+  { q: "banana since:2021-07-20", count: 10 },
+  function (err, data, response) {
+    if (err) {
+      console.log(err);
+      return;
+    } else {
+      console.log(data);
+    }
+  }
+);
 
 // set the view engine to ejs
 app.set("view engine", "ejs");
