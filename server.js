0ae6f628c99ae7ac7d5c6f6bd2176c5cd2664204
laurence <laurencehall1@outlook.com>
2021-07-28 12:30:54 -0400

    chart js work and public css fix

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const app = express();
 const port = 3000;
 const bodyParser = require("body-parser");
 const expressLayouts = require("express-ejs-layouts");
-  const TestTweetRoutes = require('./routes/TweetRoutes/TestTweetRoutes')
+const TestTweetRoutes = require("./routes/TweetRoutes/TestTweetRoutes");
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Static Files
-app.use(express.static("public"));
-// Set Templating Engine
+app.use(express.static(__dirname + '/public'));
+  // Set Templating Engine
 app.use(expressLayouts);
 app.set("layout", "./layouts/layout");
 app.set("view engine", "ejs");
@@ -23,13 +23,14 @@ app.use("/tweets", TestTweetRoutes);
 app.get("/", (req, res) => {
   res.render("pages/index");
 });
-
+app.get("/test", (req, res) => {
+  res.send({ result: "working" });
+});
 
 // about page
 app.get("/about", (req, res) => {
   res.render("pages/about");
 });
-
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
