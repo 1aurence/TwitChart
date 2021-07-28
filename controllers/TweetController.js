6ea5ad93ecb2ab841784f1667bab425b906945b7
laurence <laurencehall1@outlook.com>
2021-07-28 13:33:48 -0400

    changed page names

--- controllers/TweetController.js
+++ controllers/TweetController.js
@@ -12,15 +12,20 @@ const T = new Twit({
 
 module.exports = {
   showLengthLkesPage(req, res) {
-    res.render("../views/pages/search_tweets");
+    res.render("../views/pages/length_likes_forum");
   },
   compareLengthLikes(req, res) {
     const { keyword, amount, start_date, end_date } = req.body;
     console.log(start_date);
-    T.get("search/tweets", { q: keyword, count: amount, since_id: start_date, until: end_date})
+    T.get("search/tweets", {
+      q: keyword,
+      count: amount,
+      since_id: start_date,
+      until: end_date,
+    })
       .then((result) => {
         let data = result.data.statuses;
-        res.render("pages/search_results", { data: result.data.statuses });
+        res.render("pages/length_likes_result", { data: result.data.statuses });
       })
       .catch((err) => console.log(err));
   },
